1dbd70c0919d13cdc680a93ed65f79cd123388cc
Aryan Bhardwaj <85098435+Aryan1909@users.noreply.github.com>
2025-02-24 12:47:48 +0530

    Delay changed
    replaced delay() in all functions to the top. Verified that the power supply works after this change.

--- PSD7303B/control.py
+++ PSD7303B/control.py
@@ -39,35 +39,39 @@ my_instrument.write('CH2:curr 0.1')                 #Force the current to be 100
 def set_voltage_ch1(voltage :float) -> float:
     if not isinstance(voltage, float):
         raise TypeError("Voltage not float type")
+    delay()
     command = "CH1:VOLT "+f"{voltage:.2f}"
     print(command)
     my_instrument.write(command)
-    delay() 
+     
 
 
 def set_voltage_ch2(voltage):
     if not isinstance(voltage, float):
         raise TypeError("Voltage not float type")
+    delay()
     command = "CH2:VOLT "+f"{voltage:.2f}"
     print(command)
     my_instrument.write(command) 
-    delay()
+    
 
 def set_current_ch1(current :float) -> float:
     if not isinstance(current, float):
         raise TypeError("Current not float type")
+    delay()
     command = "CH1:CURR "+f"{current:.2f}"
     print(command)
     my_instrument.write(command)
-    delay() 
+     
 
 def set_current_ch2(current):
     if not isinstance(current, float):
         raise TypeError("Current not float type")
+    delay()
     command = "CH2:CURR "+f"{current:.2f}"
     print(command)
     my_instrument.write(command) 
-    delay()
+    
 
 def get_voltage_ch1():
     " This returns the set voltage on CH1"
@@ -89,28 +93,31 @@ def set_status_ch1(status: bool) -> bool:
     " This sets the channel status CH1: True = ON, False = Off"
     if not isinstance(status, bool):
            raise TypeError("Input not bool type")
+    delay()
     if status==True:
         my_instrument.write('OUTP CH1,ON')
     else:
         my_instrument.write('OUTP CH1,OFF')
-    delay()
+    
 
 def set_status_ch2(status: bool) -> bool:
     " This sets the channel status CH2: True = ON, False = Off"
     if not isinstance(status, bool):
         raise TypeError("Input not bool type")
+    delay()
     if status==True:
         my_instrument.write('OUTP CH2,ON')
     else:
         my_instrument.write('OUTP CH2,OFF')
-    delay()
+    
 
 def disconnect_power_supply(status: bool = True) -> bool:
     " This sets the channel status CH2: True = ON, False = Off"
     if not isinstance(status, bool):
            raise TypeError("Input not bool type")
+    delay()
     if status==True:
         my_instrument.close()
     else:
         print("Why have you written False as Arg")
-    delay()
+    
